719fdf85b5769852cab8f0bafb08f5b891fb8ccd
geronimo304 <geronimonegreira06@gmail.com>
2025-04-10 14:55:58 +0000

    arreglo en for del trabajo 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,29 +12,30 @@ void bubble_sort(int *array, size_t size)
 
 	size_t i, j;
 	int temp;
-	int inter;
+	int swapped;
 
 	if (!array || size < 2)
-		return ;
+		return;
 
-	for (j = 0; i < size - 1; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
-		inter = 0;
+		swapped = 0;
 
-		for (j = 0; size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp= array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
 				print_array(array, size);
-				inter = 1;
+				swapped = 1;
 			}
 		}
 
-		if (inter == 0)
+		if (swapped == 0)
 			break;
 	}
 }
